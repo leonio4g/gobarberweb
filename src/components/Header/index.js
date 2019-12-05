02346791158ce570da878d9a0a2d72022ed6d481
leonio santos <leoniopoint@gmail.com>
2019-12-05 09:27:40 -0300

    chore: Finaliza Aplicação do GoBarber WEB

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-roxa.svg';
 import { Link } from 'react-router-dom';
@@ -6,6 +7,9 @@ import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,10 +24,10 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Leonio Santos</strong>
+              <strong>{profile.name} </strong>
               <Link to="/profile" >Meu Perfil</Link>
             </div>
-            <img src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="Leonio Santos"/>
+            <img src={profile.avatar.url || 'https://api.adorable.io/avatars/50/abott@adorable.png'} alt="Leonio Santos"/>
           </Profile>
         </aside>
       </Content>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useDispatch ,useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
-import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
+import AvatarInput from './AvatarInput';
+import { Container } from './styles';
+
 
 export default function Profile() {
     const dispatch = useDispatch();
@@ -11,9 +14,15 @@ export default function Profile() {
     function handleSubmit(data){
       dispatch(updateProfileRequest(data));
     }
+    function handleSignOut(){
+      dispatch(signOut());
+    }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit} >
+
+
+        <AvatarInput name="avatar_id"/>
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu Email" />
 
@@ -25,7 +34,7 @@ export default function Profile() {
 
         <button type="submit">Atualizar perfil</button>
       </Form>
-        <button type="button">Sair do GoBarber</button>
+        <button type="button" onClick={handleSignOut}>Sair do GoBarber</button>
     </Container>
   );
 }
